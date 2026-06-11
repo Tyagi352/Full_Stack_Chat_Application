2b81ea0dc8fcdd0c986fca7a942db6d008b2cf4d
Tyagi352 <arnavtyagi116@gmail.com>
2026-06-11 20:20:14 +0530

    cors origin fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://full-stack-chat-application-rho.vercel.app",
     credentials: true,
   })
 );
